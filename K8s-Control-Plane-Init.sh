8db782871356141ba04f907e056d0bfd601f8982
ibtisam <174851199+ibtisam-iq@users.noreply.github.com>
2025-07-04 00:56:08 +0500

    Update K8s-Control-Plane-Init.sh

--- K8s-Control-Plane-Init.sh
+++ K8s-Control-Plane-Init.sh
@@ -103,6 +103,7 @@ echo -e "${YELLOW}     Do NOT apply the network plugin on any additional control
 echo
 
 echo -e "${BOLD}${GREEN}🚀 To complete the final setup step, run:${RESET}"
+echo
 echo -e "${GREEN}curl -sL https://raw.githubusercontent.com/ibtisam-iq/SilverInit/main/k8s-cni-setup.sh | bash${RESET}"
 echo
 
