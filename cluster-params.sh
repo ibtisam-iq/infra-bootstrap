59fa3df64145e74f5628436337ec259b3300b920
ibtisam <174851199+ibtisam-iq@users.noreply.github.com>
2025-07-03 20:49:00 +0500

    Update cluster-params.sh

--- cluster-params.sh
+++ cluster-params.sh
@@ -2,13 +2,13 @@
 # cluster-params.sh
 
 # 📡 Automatically detect Control Plane IP
-CONTROL_PLANE_IP=$(ip route get 8.8.8.8 | awk '{print $7; exit}')
+export CONTROL_PLANE_IP=$(ip route get 8.8.8.8 | awk '{print $7; exit}')
 
 # 🖥️ Get static hostname
-NODE_NAME=$(hostnamectl --static)
+export NODE_NAME=$(hostnamectl --static)
 
 # 📦 Pod CIDR (e.g., Flannel)
-POD_CIDR="10.244.0.0/16"
+export POD_CIDR="10.244.0.0/16"
 
 # 📦 Kubernetes Version
-K8S_VERSION="1.32"
+export K8S_VERSION="1.32"
