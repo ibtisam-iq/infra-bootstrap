23c6611916ae4c6407c55093310786f0c5844f2e
ibtisam <174851199+ibtisam-iq@users.noreply.github.com>
2025-07-04 00:53:52 +0500

    Update k8s-start-services.sh

--- k8s-start-services.sh
+++ k8s-start-services.sh
@@ -22,7 +22,7 @@ for service in containerd kubelet; do
     systemctl is-active "$service"
 done
 
-echo -e "\n\033[1;32m kubelet is activating, because it's waiting for the API server (which kubeadm init starts)..\033[0m"
+# echo -e "\n\033[1;32m kubelet is activating, because it's waiting for the API server (which kubeadm init starts)..\033[0m"
 # Since kubeadm init is not run, and kubelet needs a valid configuration to work, it keeps crashing and restarting.
 
 # ubuntu@ip-172-31-17-2:~$ systemctl is-active "kubelet"
