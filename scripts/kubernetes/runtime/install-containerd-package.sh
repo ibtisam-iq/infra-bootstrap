07f76c45727e6c935e2441d3da5fabc9c45fd391
Muhammad Ibtisam <174851199+ibtisam-iq@users.noreply.github.com>
2025-12-16 07:29:37 +0500

    add: init-worker-node.sh

--- scripts/kubernetes/runtime/install-containerd-package.sh
+++ scripts/kubernetes/runtime/install-containerd-package.sh
@@ -44,7 +44,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
 chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+cat /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
 Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
